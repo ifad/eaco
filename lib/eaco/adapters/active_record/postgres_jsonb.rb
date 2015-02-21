51274553e399fb4a507ef24112c181f65e006a0d
Marcello Barnaba <vjt@openssl.it>
2015-02-21 05:06:06 +0100

    Fix namespacing

--- lib/eaco/adapters/active_record/postgres_jsonb.rb
+++ lib/eaco/adapters/active_record/postgres_jsonb.rb
@@ -2,26 +2,27 @@ module Eaco
   module Adapters
     module ActiveRecord
 
-    # Authorized collection extractor on PostgreSQL >= 9.4 and a jsonb column
-    # named `acl`.
-    #
-    # Uses the json key existance operator "?|" to check whether one of the
-    # +Actor+'s +Designator+ instances exist as keys in the ACL objects.
-    #
-    # TODO negative authorizations (using a separate column?)
-    #
-    module PostgresJSONb
-
-      # Returns authorized Resource objects accessible by the given Actor.
+      # Authorized collection extractor on PostgreSQL >= 9.4 and a jsonb column
+      # named `acl`.
       #
-      def accessible_by(actor)
-        return scoped if actor.is_admin?
+      # Uses the json key existance operator "?|" to check whether one of the
+      # +Actor+'s +Designator+ instances exist as keys in the ACL objects.
+      #
+      # TODO negative authorizations (using a separate column?)
+      #
+      module PostgresJSONb
 
-        designators = actor.designators.map {|d| quote_value(d) }
+        # Returns authorized Resource objects accessible by the given Actor.
+        #
+        def accessible_by(actor)
+          return scoped if actor.is_admin?
 
-        where("acl ?| array[#{designators.join(',')}]")
+          designators = actor.designators.map {|d| quote_value(d) }
+
+          where("acl ?| array[#{designators.join(',')}]")
+        end
       end
-    end
 
+    end
   end
 end
