375e075bbba6921dc350388b8b4d095efa1d9898
Marcello Barnaba <vjt@openssl.it>
2015-02-24 03:11:30 +0100

    Rails 4.2 support

---
+++ lib/eaco/adapters/active_record/compatibility/v42.rb
@@ -0,0 +1,17 @@
+module Eaco
+  module Adapters
+    module ActiveRecord
+      class Compatibility
+
+        ##
+        # Rails 4.2 JSONB support module.
+        #
+        # Magically, this module is empty. YAY!
+        #
+        module V42
+        end
+
+      end
+    end
+  end
+end
