1205523217cadff1800f93c8b538228cdace6073
Marcello Barnaba <vjt@openssl.it>
2015-02-24 03:08:03 +0100

    Implement AR 4.0 compatibility layer

---
+++ lib/eaco/adapters/active_record/compatibility/v40.rb
@@ -0,0 +1,59 @@
+module Eaco
+  module Adapters
+    module ActiveRecord
+      class Compatibility
+
+        ##
+        # Rails v4.0.X compatibility layer for jsonb
+        #
+        module V40
+          ##
+          #
+          # Sets up the OID Type Map, reloads it, hacks native database types,
+          # and makes jsonb mimick itself as a json - for the rest of the AR
+          # machinery to work intact.
+          #
+          def self.included(base)
+            adapter = base.connection
+
+            adapter.class::OID.register_type 'jsonb', adapter.class::OID::Json.new
+            adapter.send :reload_type_map
+
+            adapter.native_database_types.update(jsonb: {name: 'json'})
+
+            adapter.class.parent::PostgreSQLColumn.instance_eval do
+              include Column
+            end
+          end
+
+          ##
+          # Patches to ActiveRecord::ConnectionAdapters::PostgreSQLColumn
+          #
+          module Column
+            ##
+            # Makes +sql_type+ return +json+ for +jsonb+ columns
+            #
+            def sql_type
+              orig_type = super
+              orig_type == 'jsonb' ? 'json' : type
+            end
+
+            ##
+            # Makes +simplified_type+ return +json+ for +jsonb+ columns
+            #
+            # @return [Symbol]
+            #
+            def simplified_type(field_type)
+              if field_type == 'jsonb'
+                :json
+              else
+                super
+              end
+            end
+          end
+        end
+
+      end
+    end
+  end
+end
