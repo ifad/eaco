66be00adeb81b58bc1e30fc7042b68131f219e08
Marcello Barnaba <vjt@openssl.it>
2015-02-28 14:52:25 +0100

    Remove pending specs now covered by features

--- spec/eaco/adapters/couchrest_model/couchdb_lucene_spec.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-require 'spec_helper'
-
-RSpec.describe Eaco::Adapters::CouchrestModel::CouchDBLucene do
-
-  pending '.accessible_by'
-
-end
--- spec/eaco/adapters/couchrest_model_spec.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-require 'spec_helper'
-
-RSpec.describe Eaco::Adapters::CouchrestModel do
-
-  pending '.included'
-
-end
