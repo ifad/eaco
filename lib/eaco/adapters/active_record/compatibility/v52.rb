fdeee312861a634f478144cdd8d09973aee511b2
Lleïr Borràs Metje <lleir@llegeix.me>
2018-10-25 16:52:54 +0200

    Add rails 5.2 and ruby 2.5.3 compatibility

---
+++ lib/eaco/adapters/active_record/compatibility/v52.rb
@@ -0,0 +1,30 @@
+module Eaco
+  module Adapters
+    module ActiveRecord
+      class Compatibility
+
+        ##
+        # Rails 5.2 support module.
+        #
+        # JSONB works correctly, but we need +.scoped+ so we revive it through
+        # the {Scoped} support module.
+        #
+        # @see Scoped
+        #
+        # Sanitize has dissapeared in favour of quote.
+        #
+        # @see Sanitized
+        #
+        module V52
+          extend ActiveSupport::Concern
+
+          included do
+            extend Scoped
+            extend Sanitized
+          end
+        end
+
+      end
+    end
+  end
+end
