2846ddd06f0d949c6fe088965e5ad03dd6f74486
Geremia Taglialatela <tagliala.dev@gmail.com>
2021-05-19 11:14:32 +0200

    Add Rails 6.1 compatibility

--- Appraisals
+++ Appraisals
@@ -38,3 +38,7 @@ end
 appraise 'rails-6.0' do
   gem 'rails', '~> 6.0.0'
 end
+
+appraise 'rails-6.1' do
+  gem 'rails', '~> 6.1.0'
+end
--- lib/eaco/adapters/active_record/compatibility.rb
+++ lib/eaco/adapters/active_record/compatibility.rb
@@ -14,6 +14,7 @@ module Eaco
         autoload :V51, 'eaco/adapters/active_record/compatibility/v51.rb'
         autoload :V52, 'eaco/adapters/active_record/compatibility/v52.rb'
         autoload :V60, 'eaco/adapters/active_record/compatibility/v60.rb'
+        autoload :V61, 'eaco/adapters/active_record/compatibility/v61.rb'
 
         autoload :Scoped,    'eaco/adapters/active_record/compatibility/scoped.rb'
         autoload :Sanitized, 'eaco/adapters/active_record/compatibility/sanitized.rb'
